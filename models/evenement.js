6d8fa39a4637199d9b0f14de13db7c25bfd7fa11
ZangaAlaeddine <150626581+ZangaAlaeddine@users.noreply.github.com>
2023-12-11 12:47:32 +0100

    Evenement.js

--- models/evenement.js
+++ models/evenement.js
@@ -1,25 +1,47 @@
-const mongoose = require("mongoose")
+const mongoose = require("mongoose");
 
-const evenement = new mongoose.Schema({
-    nom: [{
-        String
-    }],
-    date: [{
-        Date
-    }],
-    lieu: [{
-        String
-    }],
-    hote: [{
-        String
-    }],
-    description: [{
-        String
-    }],
-    image: [{
-        
-    }],
-    participants: [{
-        participantSchema
-    }],
-})
+const participantSchema = new mongoose.Schema({
+
+});
+
+const evenementSchema = new mongoose.Schema({
+  nom: {
+    type: String,
+    required: true,
+  },
+  date: {
+    type: Date,
+    required: true,
+  },
+  lieu: {
+    type: String,
+    required: true,
+  },
+  hote: {
+    type: String,
+    required: true,
+  },
+  description: {
+    type: String,
+    required: true,
+  },
+  image: {
+   
+  },
+  participants: {
+    type: [participantSchema],
+  },
+ 
+  event_date: {
+    type: Date,
+    required: true,
+  },
+  archived: {
+    type: Boolean,
+    default: false,
+  },
+});
+
+const Evenement = mongoose.model("Evenement", evenementSchema);
+
+module.exports = Evenement;
